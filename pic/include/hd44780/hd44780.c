33056a712849a7391e70a729c2bd715c49240d7d
Kamilcuk <kamilcukrowski@gmail.com>
2017-05-01 01:40:26 +0200

    Change licencese

--- pic/include/hd44780/hd44780.c
+++ pic/include/hd44780/hd44780.c
@@ -2,21 +2,8 @@
  * hd44780.c - communicate with hd44780 compatible controllers
  * look in hd44780-config.h for more information!
  * 
- * Copyright 2013 Kamil Cukrowski <kamil@dyzio.pl>
+ * Under BEER-WARE license 2013 Kamil Cukrowski <kamil@dyzio.pl>
  * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 2.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
  * 
  * 
  */
